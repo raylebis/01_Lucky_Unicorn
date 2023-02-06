87c840e447446bf13225b6bbd4512c2645f81632
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-07 12:45:36 +1300

    Update 00_sandbox.py

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,12 +1,5 @@
-# Ask users for a number
+# Count up from one to 10
 
-get_number = int(input("Choose a number? "))
-# Multiply the number by 5
-
-times_five = get_number * 5
-answer = "{} times five is equal to " \
-         "{}".format(get_number, times_five)
-# Output the result
-
-print(answer)
+for item in range(1, 11):
+    print(item)
 
