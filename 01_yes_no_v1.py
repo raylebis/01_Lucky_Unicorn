fc0fd5cdb560131a95d9ae2e302d355bf4032dff
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-07 13:34:07 +1300

    Update 01_yes_no_v1.py

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,16 +1,24 @@
 # Ask the user if they have played before
-show_instructions = input("Have you played this game before? ").lower()
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+    show_instructions = input("Have you played this game before? ").lower()
 
 # If they say yes, output 'program continues'
 
-if show_instructions == "yes":
-    print("program continues")
+    if show_instructions == "yes":
+        print("program continues")
 
-elif show_instructions == "no":
-    print("display instructions")
+    elif show_instructions == "y":
+        print("program continues")
+
+    elif show_instructions == "no":
+        print("display instructions")
+
+    elif show_instructions == "n":
+        print("display instructions")
 
 
 # If they say no, output 'display instructions'
-else:
-    print("Display instructions")
+    else:
+        print("Display instructions")
 
