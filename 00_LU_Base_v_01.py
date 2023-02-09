6647debf8ce03d529d904e0b7dc8b55f9958b735
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-09 13:34:17 +1300

    Update 00_LU_Base_v_01.py

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -15,12 +15,14 @@ def yes_no(question):
         else:
             print("Please enter either 'Yes' or 'No'")
 
+
 def instructions():
     print("*** How to Play ***")
     print()
     print("The rules of the game goes here")
     print()
     return""
+
 
 def num_check(question, low, high):
 
@@ -41,6 +43,7 @@ def num_check(question, low, high):
         except ValueError:
             print(error)
 
+
 # Main routine
 played_before = yes_no("Have you played the "
                            "game before? ")
@@ -48,3 +51,6 @@ if played_before == "no":
     instructions()
 
 print("program continues")
+
+how_much = num_check("How much would you like to play with? ",0,10)
+print("You have asked to play with ${}".format(how_much))
