931aba1afef384b0e95ded7460d9df9fc5d785d1
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-13 12:08:39 +1300

    added token generator

---
+++ 05_token_generator_v2.py
@@ -0,0 +1,22 @@
+import random
+
+#main routine goes here
+
+tokens = ["unicorn", "horse", "zebra", "donkey"]
+balance = 100
+
+# Testing loop to generate 20 tokens
+for item in range(0, 20):
+    chosen = random.choice(tokens)
+    print(chosen, end='\t')
+
+    # Adjust balance
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.50
+
+# output
+print("Toke: {} , Balance: ${}".format(chosen, balance))
