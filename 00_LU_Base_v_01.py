7372bd2dd5d343d91e15a317574099e1c429fd08
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-13 17:14:54 +1300

    Update 00_LU_Base_v_01.py

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -56,6 +56,7 @@ print("program continues")
 how_much = num_check("How much would you like to play with? ",0,10)
 print("You have asked to play with ${}".format(how_much))
 
+# set balance for testing purposes
 balance = how_much
 
 rounds_played = 0
@@ -63,39 +64,33 @@ rounds_played = 0
 play_again = input("press <Enter> to play...").lower()
 while play_again == "":
 
-    # increase # of rounds played
+    # increases the number of rounds being played
     rounds_played += 1
 
-    # print round number
+    # prints the round number
     print()
     print("*** Round #{} ***".format(rounds_played))
-
     chosen_num = random.randint(1, 100)
 
     # Adjust balance
-    # if the random #is between 1 and 5,
-    # user gets a unicorn (add $4 to balance)
+    # if the random # is between 1 and 5, the user gets a unicorn (adding $4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
-
-    # if the random# is between 6 and 36
-    # user gets a donkey (subtract $1 from balance)
+        # if the random # is between 6 and 36, user gets a donkey (subtract $1 from balance)
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
-    # The token is either a horse or zebra...
-    # in both cases, subtract $0.50 from the balance
     else:
-        # if the number is eve, set the chosen
-        # item to a horse
+        # if the number is even, set the chosen item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse"
-
-        # otherwise set it to a zebra
+            # otherwise set it to a zebra
         else:
             chosen = "zebra"
         balance -= 0.5
+
+    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
 
     if balance < 1:
         play_again = "xxx"
@@ -103,8 +98,6 @@ while play_again == "":
     else:
         play_again = input("Press Enter to play again "
                            "or 'xxx' to quit")
-
-    print("You got a {}.  Balance: ${:.2f}".format(chosen, balance))
 
 print()
 print("Final balance", balance)
