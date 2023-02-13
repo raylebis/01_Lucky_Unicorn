9f7c29891708be16cbbd4db7cf87803f69c37305
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-13 18:53:35 +1300

    Finished

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -45,15 +45,71 @@ def num_check(question, low, high):
             print(error)
 
 
+def statement_generator(statement, decoration):
+
+    sides = decoration * 3
+
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
+
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
+
+
+def instructions():
+
+    statement_generator("How to Play", "*")
+    print()
+    print("Choose a starting amount (minimum $1, maximum $10")
+    print()
+    print("Then press <enter> to play. You will get either a horse, a zebra, a donkey or a unicorn")
+    print()
+    print("It costs $1 per round. Depending your prize you might win some of the money back. Heres the payouts amounts...")
+    print("Unicorn: $5.00 (balance increases by $4")
+    print("Horse: -$0.50 (balance decreases by $0.50")
+    print("Zebra: -$0.50 (balance decreases by $0.50")
+    print("Donkey: $1.00 (balance decreases by $1.00 ")
+    print()
+    print("Do you think you have what it takes to win this game? and take advantage?")
+
+    return ""
+
+
+
+def statement_generator(outcome, prize_decoration):
+
+    sides = prize_decoration * 3
+
+    outcome = "{} {} {}".format(sides, outcome, sides)
+    top_bottom = prize_decoration * len(outcome)
+
+    print(top_bottom)
+    print(outcome)
+    print(top_bottom)
+
+    return ""
+
+
+
 # Main routine
+statement_generator("Welcome to the Lucky Unicorn Game", "*")
+
 played_before = yes_no("Have you played the "
                            "game before? ")
+
+
 if played_before == "no":
     instructions()
 
-print("program continues")
+if played_before == "yes":
+    statement_generator("Lets get started", "+")
 
-how_much = num_check("How much would you like to play with? ",0,10)
+print()
+
+how_much = num_check("How much would you like to play with? ", 0, 10)
 print("You have asked to play with ${}".format(how_much))
 
 # set balance for testing purposes
@@ -76,21 +132,27 @@ while play_again == "":
     # if the random # is between 1 and 5, the user gets a unicorn (adding $4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
+        prize_decoration = "!"
         balance += 4
         # if the random # is between 6 and 36, user gets a donkey (subtract $1 from balance)
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
+        prize_decoration = "D"
         balance -= 1
     else:
         # if the number is even, set the chosen item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse"
-            # otherwise set it to a zebra
+            prize_decoration = "H"
+        # otherwise set it to a zebra
         else:
             chosen = "zebra"
+            prize_decoration = "Z"
         balance -= 0.5
 
-    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+    outcome = "You got a {}. Your balance is ${:.2f}".format(chosen, balance)
+
+    statement_generator(outcome, prize_decoration)
 
     if balance < 1:
         play_again = "xxx"
--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,5 +1,1 @@
-import random
-
-for item in range(0, 20):
-    number = random.randint(0, 4)
-    print(number, end="\t")
+print("$" * 3)
